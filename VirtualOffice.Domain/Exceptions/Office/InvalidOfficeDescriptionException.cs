56949aa155350106f266d766aa09265e4f11c642
Szczupi285 <bartekbiexd@gmail.com>
2024-03-24 20:37:11 +0100

    Added OfficeDescription value object and exceptions for self validating purpose

---
+++ VirtualOffice.Domain/Exceptions/Office/InvalidOfficeDescriptionException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Shared.Abstractions.Exceptions;
+
+namespace VirtualOffice.Domain.Exceptions.Office
+{
+    public class InvalidOfficeDescriptionException : VirtualOfficeException
+    {
+        string Value;
+        public InvalidOfficeDescriptionException(string value) : base($"Description: {value} is more than 200 characters long")
+        {
+        }
+    }
+}
---
+++ VirtualOffice.Domain/ValueObjects/Office/OfficeDescription.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Domain.Exceptions.Office;
+
+namespace VirtualOffice.Domain.ValueObjects.Office
+{
+    public sealed record OfficeDescription
+    {
+        public string Value { get; }
+
+        public OfficeDescription(string value)
+        {
+
+            if (value.Length > 200)
+                throw new EmptyOfficeNameException();
+            else if(value is null)
+                throw new OfficeDescriptionIsNullException();
+
+            Value = value;
+        }
+
+        public static implicit operator string(OfficeDescription name)
+            => name.Value;
+
+        public static implicit operator OfficeDescription(string name)
+            => new(name);
+    }
+}
+}
