9d475350c3ff55c5b5b5397ad993f54044029be9
Szczupi285 <bartekbiexd@gmail.com>
2024-03-27 21:31:02 +0100

    Added unit tests for ApplicationUserSurname

--- DomainUnitTests/ApplicationUserUnitTests.cs
+++ DomainUnitTests/ApplicationUserUnitTests.cs
@@ -14,7 +14,7 @@ namespace DomainUnitTests
                 => new ApplicationUserId(Guid.Empty));
         }
         [Fact]
-        public void ValidApplicationUserId_StringsShouldMatch()
+        public void ValidApplicationUserId_ValidGuidToAppUserIdConversion_ShouldEqual()
         {
             var guid = Guid.NewGuid();
 
@@ -23,7 +23,7 @@ namespace DomainUnitTests
             Assert.Equal(id.Value, guid);
         }
         [Fact]
-        public void ValidApplicationUserId_ValidConversion()
+        public void ValidApplicationUserId_ValidAppUserIdToGuidConversionShouldEqual()
         {
 
             ApplicationUserId id = new ApplicationUserId(Guid.NewGuid());
@@ -35,6 +35,27 @@ namespace DomainUnitTests
         #endregion
 
         #region ApplicationUserName
+
+        [Fact]
+        public void ValidApplicationUserName_ValidStringToAppUserNameConversion_ShouldEqual()
+        {
+            string name = "Michael";
+
+            ApplicationUserName nam = name;
+
+            Assert.Equal(name, nam);
+        }
+        [Fact]
+        public void ValidApplicationUserName_ValidAppUserNameToStringConversionShouldEqual()
+        {
+            ApplicationUserName nam = new ApplicationUserName("Michael");
+
+            string name = nam;
+
+            Assert.Equal(nam, name);
+
+        }
+
         [Fact]
         public void EmptyApplicationUserName_ShouldReturnEmptyApplicationUserNameException()
         {
@@ -118,7 +139,111 @@ namespace DomainUnitTests
 
         #endregion
 
-        
+        #region ApplicationUserSurname
+
+        [Fact]
+        public void EmtpyApplicationUserSurname_ShouldReturnEmptyApplicationUserSurnameException()
+        {
+            Assert.Throws<EmptyApplicationUserSurnameException>(()
+                => new ApplicationUserSurname(""));
+        }
+        [Fact]
+        public void NullApplicationUserSurname_ShouldReturnEmptyApplicationUserSurnameException()
+        {
+            Assert.Throws<EmptyApplicationUserSurnameException>(()
+                => new ApplicationUserSurname(null));
+        }
+        [Theory]
+        // 30char
+        [InlineData("Thissurnameislongerthancharacterssss")]
+        // 50 char
+        [InlineData("ThissurnameislongerthancharactersThisusernameisllll")]
+        public void TooLongApplicationUserSurname_ShouldReturnTooLongApplicationUserSurnameException(string surname)
+        {
+            Assert.Throws<TooLongApplicationUserSurnameException>(()
+                => new ApplicationUserSurname(surname));
+        }
+        [Theory]
+        [InlineData("Validsurname")]
+        [InlineData("Anothersurname")]
+        // 30char
+        [InlineData("Thissurnameisexactlycharactetr")]
+        public void ValidApplicationUserSurname_StringsShouldMatch(string surname)
+        {
+            ApplicationUserSurname sur = new ApplicationUserSurname(surname);
+
+            Assert.Equal(surname, sur);
+        }
+        [Fact]
+        public void ValidApplicationUserSurname_ValidStringToAppUserSurnameConversion_ShouldEqual()
+        {
+            string surname = "Examplesurname";
+
+            ApplicationUserSurname sur = surname;
+
+            Assert.Equal(surname, sur);
+        }
+        [Fact]
+        public void ValidApplicationUserSurname_ValidAppUserSurnameTostringConversionShouldEqual()
+        {
+            ApplicationUserSurname sur = new ApplicationUserSurname("Examplesurname");
+
+            string surname = sur;
+
+            Assert.Equal(sur, surname);
+
+        }
+
+        [Theory]
+        [InlineData("0")]
+        [InlineData("1")]
+        [InlineData("2")]
+        [InlineData("3")]
+        [InlineData("4")]
+        [InlineData("5")]
+        [InlineData("6")]
+        [InlineData("7")]
+        [InlineData("8")]
+        [InlineData("9")]
+        [InlineData("!")]
+        [InlineData("@")]
+        [InlineData("#")]
+        [InlineData("$")]
+        [InlineData("%")]
+        [InlineData("^")]
+        [InlineData("&")]
+        [InlineData("*")]
+        [InlineData("(")]
+        [InlineData(")")]
+        [InlineData("-")]
+        [InlineData("_")]
+        [InlineData("+")]
+        [InlineData("=")]
+        [InlineData("{")]
+        [InlineData("}")]
+        [InlineData("[")]
+        [InlineData("]")]
+        [InlineData("|")]
+        [InlineData("\\")]
+        [InlineData(":")]
+        [InlineData(";")]
+        [InlineData("'")]
+        [InlineData("<")]
+        [InlineData(">")]
+        [InlineData(",")]
+        [InlineData(".")]
+        [InlineData("?")]
+        [InlineData("/")]
+        [InlineData("`")]
+        [InlineData("~")]
+        public void InvalidApplicationUserSurnameShorterThan36char_ShouldReturnInvalidApplicationUserSurnameException(string surname)
+        {
+            Assert.Throws<InvalidApplicationUserSurnameException>(()
+                => new ApplicationUserSurname(surname));
+        }
+
+        #endregion
+
     }
 
 }
