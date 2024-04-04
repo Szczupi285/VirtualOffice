3e3752cb06f100bf476f0d8c39bc49c7e905c445
Szczupi285 <bartekbiexd@gmail.com>
2024-04-04 09:49:03 +0200

    Refactored ApplicationUserName

--- VirtualOffice.Domain/ValueObjects/ApplicationUser/ApplicationUserName.cs
+++ VirtualOffice.Domain/ValueObjects/ApplicationUser/ApplicationUserName.cs
@@ -1,28 +1,21 @@
-﻿using VirtualOffice.Domain.Exceptions.ApplicationUser;
+﻿using VirtualOffice.Domain.Abstractions;
+using VirtualOffice.Domain.Exceptions.ApplicationUser;
+using VirtualOffice.Domain.Exceptions.Office;
 
 namespace VirtualOffice.Domain.ValueObjects.ApplicationUser
 {
-    public sealed record ApplicationUserName
+    public sealed record ApplicationUserName : AbstractRecordName
     {
-        public string Value { get; }
 
-        public ApplicationUserName(string value)
+        public ApplicationUserName(string value) : base(value, 30, new EmptyApplicationUserNameException(), new TooLongApplicationUserNameException(value))
         {
-            if (string.IsNullOrWhiteSpace(value))
-                throw new EmptyApplicationUserNameException();
-            else if (value.Length > 30)
-                throw new TooLongApplicationUserNameException(value);
             // Since there is for example 1 letter chinese names we only require at least 1 letter
             // dot is still valid so we can allow abbreviations 
-            else if (!value.All(c => char.IsLetter(c) || c == '.') 
+            if (!value.All(c => char.IsLetter(c) || c == '.')
                 && !value.Any(char.IsLetter) || value[0] == '.')
                 throw new InvalidApplicationUserNameException(value);
 
-            Value = value.Trim();
         }
-
-        public static implicit operator string(ApplicationUserName name)
-            => name.Value;
 
         public static implicit operator ApplicationUserName(string name)
             => new(name);
