08e88cb836f28a7102c33653a319cb0252e7ade0
unknown <bartekbiexd@gmail.com>
2024-05-06 13:44:55 +0200

    Created Premission Service and PermissionDto

---
+++ VirtualOffice.Domain/Services/PermissionService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Domain.Consts;
+using VirtualOffice.Domain.Dto;
+
+namespace VirtualOffice.Domain.Services
+{
+    public class PermissionService
+    {
+        private PermissionsEnum _PermissionsEnum { get; }
+        public PermissionService(PermissionsEnum permissions)
+        {
+            _PermissionsEnum = permissions;
+        }
+
+        public bool HasPermission(PermissionsEnum permission) => _PermissionsEnum.HasFlag(permission);
+        public PermissionsEnum GetAllPermissions() => _PermissionsEnum;
+
+
+
+    }
+}
