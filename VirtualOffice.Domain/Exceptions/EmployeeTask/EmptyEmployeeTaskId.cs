49885e84a8fb4fe581387c26229af1beae33370e
unknown <bartekbiexd@gmail.com>
2024-05-08 19:52:53 +0200

    Created EmployeeTask and implemeted few valaue objects. Commented ones in EmployeeTask are in progress

---
+++ VirtualOffice.Domain/Exceptions/EmployeeTask/EmptyEmployeeTaskId.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Shared.Abstractions.Exceptions;
+
+namespace VirtualOffice.Domain.Exceptions.EmployeeTask
+{
+    public class EmptyEmployeeTaskId : VirtualOfficeException
+    {
+        public EmptyEmployeeTaskId() 
+            : base("EmployeeTask Id cannot be empty")
+        {
+        }
+    }
+}
---
+++ VirtualOffice.Domain/ValueObjects/EmployeeTask/EmptyEmployeeTaskIdException.cs
@@ -0,0 +1,8 @@
+﻿using VirtualOffice.Shared.Abstractions.Exceptions;
+
+namespace VirtualOffice.Domain.ValueObjects.EmployeeTask
+{
+    internal class EmptyEmployeeTaskIdException : VirtualOfficeException
+    {
+    }
+}
