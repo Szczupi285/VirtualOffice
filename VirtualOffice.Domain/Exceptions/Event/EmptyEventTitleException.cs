475685fa91cfbfc74d186d5bfac8aa5fb6c29cae
unknown <bartekbiexd@gmail.com>
2024-05-12 19:14:37 +0200

    Implemented EventTitle Value Object

---
+++ VirtualOffice.Domain/Exceptions/Event/EmptyEventTitleException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Shared.Abstractions.Exceptions;
+
+namespace VirtualOffice.Domain.Exceptions.Event
+{
+    public class EmptyEventTitleException : VirtualOfficeException
+    {
+        public EmptyEventTitleException() : base("Event title cannot be empty")
+        {
+        }
+    }
+}
--- VirtualOffice.Domain/ValueObjects/Event/EventTitle.cs
+++ VirtualOffice.Domain/ValueObjects/Event/EventTitle.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VirtualOffice.Domain.Abstractions;
+using VirtualOffice.Domain.Exceptions.EmployeeTask;
+using VirtualOffice.Domain.Exceptions.Event;
+using VirtualOffice.Domain.ValueObjects.EmployeeTask;
 
 namespace VirtualOffice.Domain.ValueObjects.Event
 {
-    public sealed record EventTitle
+    public sealed record EventTitle : AbstractRecordName
     {
+        public EventTitle(string value) : base(value, 100, new EmptyEventTitleException(), new TooLongEventTitleException(value, 100))
+        {
+        }
+
+        public static implicit operator EventTitle(string content)
+            => new(content);
     }
 }
