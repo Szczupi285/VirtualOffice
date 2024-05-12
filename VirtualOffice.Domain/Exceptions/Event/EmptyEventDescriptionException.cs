fd255e4d910e092905d8a9e0e79583f5e7156492
unknown <bartekbiexd@gmail.com>
2024-05-12 19:16:35 +0200

    Implemented EventDescription

---
+++ VirtualOffice.Domain/Exceptions/Event/EmptyEventDescriptionException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Shared.Abstractions.Exceptions;
+
+namespace VirtualOffice.Domain.Exceptions.Event
+{
+    public class EmptyEventDescriptionException : VirtualOfficeException
+    {
+        public EmptyEventDescriptionException() : base("Event Description cannot be empty")
+        {
+        }
+    }
+}
--- VirtualOffice.Domain/ValueObjects/Event/EventDescription.cs
+++ VirtualOffice.Domain/ValueObjects/Event/EventDescription.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VirtualOffice.Domain.Abstractions;
+using VirtualOffice.Domain.Exceptions.Event;
 
 namespace VirtualOffice.Domain.ValueObjects.Event
 {
-    public sealed record EventDescription
+    public sealed record EventDescription : AbstractRecordName
     {
+        public EventDescription(string value) : base(value, 100, new EmptyEventDescriptionException(), new TooLongEventDescriptionException(value, 100))
+        {
+        }
+
+        public static implicit operator EventDescription(string content)
+            => new(content);
     }
 }
