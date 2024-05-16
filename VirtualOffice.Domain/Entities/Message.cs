b2c1e4f79fd84f6164801e40b6e4467113ab4a68
unknown <bartekbiexd@gmail.com>
2024-05-16 15:05:02 +0200

    Created message entity

---
+++ VirtualOffice.Domain/Entities/Message.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Domain.ValueObjects.Message;
+
+namespace VirtualOffice.Domain.Entities
+{
+    public class Message
+    {
+        public MessageId Id { get; }
+
+        public ApplicationUser _Sender {  get; }
+
+        public DateTime _SendDate 
+        {
+            get => DateTime.UtcNow;
+        }
+
+        public MessageContent _Content {get;}
+
+
+        public Message(MessageId id, ApplicationUser sender, MessageContent content)
+        {
+            Id = id;
+            _Sender = sender;
+            _Content = content;
+        }
+    }
+}
---
+++ VirtualOffice.Domain/ValueObjects/Message/MessageContent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Domain.Abstractions;
+using VirtualOffice.Domain.Exceptions.Message;
+
+namespace VirtualOffice.Domain.ValueObjects.Message
+{
+    public sealed record MessageContent : AbstractRecordName
+    {
+        public MessageContent(string value) : base(value, 500, new EmptyMessageContentException(), new TooLongMessageContentException(value, 500))
+        {
+        }
+
+        public static implicit operator MessageContent(string content)
+            => new(content);
+    }
+}
