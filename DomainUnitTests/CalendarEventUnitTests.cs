38b3ad506bf710f390cfd3c823f80f96725ab080
unknown <bartekbiexd@gmail.com>
2024-05-16 14:41:14 +0200

    Created Unit tests for methods from AbstractEvent in CalendarEvent concrete class

--- DomainUnitTests/CalendarEventUnitTests.cs
+++ DomainUnitTests/CalendarEventUnitTests.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VirtualOffice.Domain.Abstractions;
+using VirtualOffice.Domain.Entities;
 using VirtualOffice.Domain.Exceptions.Event;
 using VirtualOffice.Domain.ValueObjects.Event;
 
@@ -10,6 +12,21 @@ namespace DomainUnitTests
 {
     public class CalendarEventUnitTests
     {
+        public CalendarEvent _CalendarEvent { get; set; }
+        public ApplicationUser User { get; set; }
+        public CalendarEventUnitTests()
+        {
+            var eventId = new EventId(Guid.NewGuid());
+            var title = new EventTitle("Sample Event");
+            var startDate = new EventStartDate(DateTime.UtcNow);
+            var endDate = new EventEndDate(DateTime.UtcNow.AddDays(1));
+            var description = new EventDescription("This is a sample event description");
+            User = new ApplicationUser(Guid.NewGuid(), "John", "Doe");
+            var visibleTo = new List<ApplicationUser> { User };
+
+            _CalendarEvent = new CalendarEvent(eventId, title, startDate, endDate, description, visibleTo);
+        }
+
         #region EventId
 
         [Fact]
@@ -231,6 +248,148 @@ namespace DomainUnitTests
 
 
             Assert.Equal(EndDate, dt);
+        }
+        #endregion
+
+        #region Methods
+        [Fact]
+        public void EditTitle_SetsTitle()
+        {
+           
+            string newTitle = "New Title";
+
+            _CalendarEvent.EditTitle(newTitle);
+
+            Assert.Equal(newTitle, _CalendarEvent._Title);
+        }
+
+        [Fact]
+        public void EditDescription_SetsDescription()
+        {
+            string newDescription = "New Description";
+
+            _CalendarEvent.EditDescription(newDescription);
+
+            Assert.Equal(newDescription, _CalendarEvent._Description);
+        }
+
+        [Fact]
+        public void EditStartDate_SetsStartDate()
+        {
+            DateTime startDate = DateTime.Now;
+
+            _CalendarEvent.EditStartDate(startDate);
+
+            Assert.Equal(startDate, _CalendarEvent._StartDate);
+        }
+
+        [Fact]
+        public void EditEndDate_SetsEndDate()
+        {
+            DateTime endDate = DateTime.Now;
+
+            _CalendarEvent.EditEndDate(endDate);
+
+            Assert.Equal(endDate, _CalendarEvent._EndDate);
+        }
+
+        [Fact]
+        public void AddUserToVisibleTo_AddsUser()
+        {
+            var userId = Guid.NewGuid();
+            var user = new ApplicationUser(userId, "ExampleName", "ExampleSurname");
+
+            _CalendarEvent.AddUserToVisibleTo(user);
+
+            Assert.Contains(user, _CalendarEvent._VisibleTo);
+        }
+
+        [Fact]
+        public void AddUserToVisibleTo_ThrowsExceptionIfUserAlreadyExists()
+        {
+            var userId = Guid.NewGuid();
+
+            Assert.Throws<ThisEventIsAlreadyVIsibleToThisUser>(() => _CalendarEvent.AddUserToVisibleTo(User));
+        }
+
+        [Fact]
+        public void AddUsersRangeToVisibleTo_AddsUsers()
+        {
+            var users = new List<ApplicationUser>
+            {
+                new ApplicationUser(Guid.NewGuid(), "ExampleName", "ExampleSurname"),
+                new ApplicationUser(Guid.NewGuid(), "ExampleName", "ExampleSurname")
+           
+            };
+
+            _CalendarEvent.AddUsersRangeToVisibleTo(users);
+
+            Assert.Equal(users.Count + 1, _CalendarEvent._VisibleTo.Count);
+            foreach (var user in users)
+            {
+                Assert.Contains(user, _CalendarEvent._VisibleTo);
+            }
+        }
+        [Fact]
+        public void AddUsersRangeToVisibleTo_ShouldThrowThisEventIsAlreadyVisibleToThisUser()
+        {
+            var users = new List<ApplicationUser>
+            {
+                new ApplicationUser(Guid.NewGuid(), "ExampleName", "ExampleSurname"),
+                new ApplicationUser(Guid.NewGuid(), "ExampleName", "ExampleSurname"),
+                User
+
+            };
+            
+            Assert.Throws<ThisEventIsAlreadyVIsibleToThisUser>(() => _CalendarEvent.AddUsersRangeToVisibleTo(users));
+        }
+
+        [Fact]
+        public void RemoveUserFromVisibleTo_RemovesUser()
+        {
+            _CalendarEvent.RemoveUserFromVisibleTo(User);
+
+            Assert.DoesNotContain(User, _CalendarEvent._VisibleTo);
+        }
+
+        [Fact]
+        public void RemoveUserFromVisibleTo_ThrowsExceptionIfUserNotFound()
+        {
+            var testUser = new ApplicationUser(Guid.NewGuid(), "ExampleName", "ExampleSurname");
+            Assert.Throws<UserIsNotFoundInVisibleToCollection>(() => _CalendarEvent.RemoveUserFromVisibleTo(testUser));
+        }
+
+        [Fact]
+        public void RemoveUsersRangeFromVisibleTo_RemovesUsers()
+        {
+            var users = new List<ApplicationUser>
+            {
+                new ApplicationUser(Guid.NewGuid(), "ExampleName", "ExampleSurname"),
+                new ApplicationUser(Guid.NewGuid(), "ExampleName", "ExampleSurname")
+
+            };
+            _CalendarEvent.AddUsersRangeToVisibleTo(users);
+
+            _CalendarEvent.RemoveUsersRangeFromVisibleTo(users);
+
+            foreach (var user in users)
+            {
+                Assert.DoesNotContain(user, _CalendarEvent._VisibleTo);
+            }
+        }
+
+        [Fact]
+        public void RemoveUsersRangeFromVisibleTo_ShouldThrowUserIsNotFoundInVisibleToCollection()
+        {
+            var users = new List<ApplicationUser>
+            {
+                new ApplicationUser(Guid.NewGuid(), "ExampleName", "ExampleSurname"),
+                new ApplicationUser(Guid.NewGuid(), "ExampleName", "ExampleSurname"),
+                User
+
+            };
+
+            Assert.Throws<UserIsNotFoundInVisibleToCollection>(() => _CalendarEvent.RemoveUsersRangeFromVisibleTo(users));
         }
         #endregion
     }
