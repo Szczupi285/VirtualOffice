24bfb21305fbc9a5c61018baaeab6a05ac3ba91c
unknown <bartekbiexd@gmail.com>
2024-05-27 16:14:54 +0200

    Created IMeetingRepository

---
+++ VirtualOffice.Domain/Repositories/IMeetingRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Domain.Entities;
+
+namespace VirtualOffice.Domain.Repositories
+{
+    public interface IMeetingRepository
+    {
+        Meeting GetById(Guid guid);
+        void Add(Meeting user);
+        void Update(Meeting user);
+        void Delete(Guid id);
+        IEnumerable<Meeting> GetAllForUser(Guid userId);
+        IEnumerable<Meeting> GetAllForUserFutureEvents(Guid userId);
+        IEnumerable<Meeting> GetAllForUserByDate(Guid userId, DateTime startDate, DateTime endDate);
+    }
+}
