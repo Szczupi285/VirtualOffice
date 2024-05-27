6387887dd573c992c55d44d365af2f0f1f64618d
unknown <bartekbiexd@gmail.com>
2024-05-27 16:05:36 +0200

    Created ICalendarEventRepository

---
+++ VirtualOffice.Domain/Repositories/ICalendarEventRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Domain.Entities;
+
+namespace VirtualOffice.Domain.Repositories
+{
+    public interface ICalendarEventRepository
+    {
+        CalendarEvent GetById(Guid guid);
+        void Add(CalendarEvent user);
+        void Update(CalendarEvent user);
+        void Delete(Guid id);
+        IEnumerable<CalendarEvent> GetAllForUser(Guid userId);
+        IEnumerable<CalendarEvent> GetAllForUserFutureEvents(Guid userId);
+        IEnumerable<CalendarEvent> GetAllForUserInCertainPeriod(Guid userId, DateTime startDate, DateTime endDate);
+    }
+}
