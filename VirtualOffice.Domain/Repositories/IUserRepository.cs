1ba65a0a5170e0fae3e0e791ce3e6960bde2474b
unknown <bartekbiexd@gmail.com>
2024-05-27 15:42:48 +0200

    Created IUserRepository

---
+++ VirtualOffice.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Domain.Entities;
+
+namespace VirtualOffice.Domain.Repositories
+{
+    public interface IUserRepository
+    {
+        ApplicationUser GetById(Guid id);
+        void Add(ApplicationUser user);
+        void Update(ApplicationUser user);
+        void Delete(Guid id);
+    }
+}
