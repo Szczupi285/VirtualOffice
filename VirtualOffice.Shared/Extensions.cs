70aa8bc48e5dfad9d095a6f7cddbf2dd661a8ec4
unknown <bartekbiexd@gmail.com>
2024-06-16 17:17:07 +0200

    Created Extension static class that allows us to AddCommands

---
+++ VirtualOffice.Shared/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Shared.Abstractions.Commands;
+using VirtualOffice.Shared.Commands;
+
+namespace VirtualOffice.Shared
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddCommands(this IServiceCollection services)
+        {
+            services.AddScoped<ICommandDispatcher, MemoryCommandDispatcher>();
+            return services;
+        }
+    }
+}
