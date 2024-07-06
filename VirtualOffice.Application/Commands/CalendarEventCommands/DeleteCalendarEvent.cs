2bade830715fb86c2e51b23775843eb18b72842d
unknown <bartekbiexd@gmail.com>
2024-07-06 13:42:42 +0200

    Created RemoveCalendarEventCommand

---
+++ VirtualOffice.Application/Commands/CalendarEventCommands/DeleteCalendarEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Shared.Abstractions.Commands;
+
+namespace VirtualOffice.Application.Commands.CalendarEventCommands
+{
+    public record DeleteCalendarEvent(Guid guid) : ICommand;
+
+}
---
+++ VirtualOffice.Application/Commands/Handlers/DeleteCalendarEventHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Application.Commands.CalendarEventCommands;
+using VirtualOffice.Application.Exceptions;
+using VirtualOffice.Application.Services;
+using VirtualOffice.Domain.Entities;
+using VirtualOffice.Domain.Repositories;
+using VirtualOffice.Shared.Abstractions.Commands;
+
+namespace VirtualOffice.Application.Commands.Handlers
+{
+    public class DeleteCalendarEventHandler : ICommandHandler<DeleteCalendarEvent>
+    {
+        private readonly ICalendarEventRepository _repository;
+        private readonly ICalendarEventReadService _readService;
+
+        public DeleteCalendarEventHandler(ICalendarEventRepository repository, ICalendarEventReadService readService)
+        {
+            _repository = repository;
+            _readService = readService;
+        }
+
+        public async Task HandleAsync(DeleteCalendarEvent command, CancellationToken cancellationToken)
+        {
+
+            if (!await _readService.ExistsByIdAsync(command.guid))
+            {
+                throw new CalendarEventDoesNotExistException(command.guid);
+            }
+
+            var calEv = _repository.Delete(command.guid);
+        }
+    }
+}
