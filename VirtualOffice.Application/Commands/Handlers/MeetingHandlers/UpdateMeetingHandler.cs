c0d017374f369ad3204021ddeee55493139e2680
unknown <gosgie02@wp.pl>
2024-07-06 23:15:58 +0200

    Created UpdateCalendarEvent command

---
+++ VirtualOffice.Application/Commands/Handlers/MeetingHandlers/UpdateMeetingHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Application.Commands.MeetingCommands;
+using VirtualOffice.Application.Exceptions.Meeting;
+using VirtualOffice.Application.Services;
+using VirtualOffice.Domain.Entities;
+using VirtualOffice.Domain.Repositories;
+using VirtualOffice.Shared.Abstractions.Commands;
+
+namespace VirtualOffice.Application.Commands.Handlers.MeetingHandlers
+{
+    public class UpdateMeetingHandler : ICommandHandler<UpdateMeeting>
+    {
+        public IMeetingRepository _repository;
+        public IMeetingEventReadService _readService;
+
+        public UpdateMeetingHandler(IMeetingRepository repository, IMeetingEventReadService eventReadService)
+        {
+            _repository = repository;
+            _readService = eventReadService;
+        }
+
+        public async Task HandleAsync(UpdateMeeting command, CancellationToken cancellationToken)
+        {
+            var (id, title, description, startDate, endDate) = command;
+
+            if (await _readService.ExistsByIdAsync(id))
+            {
+                throw new MeetingDoesNotExistException(id);
+            }
+
+            var meeting = _repository.GetById(id);
+
+            if(meeting._Title != title)
+                meeting.SetTitle(title);
+            if(meeting._Description != description)
+                meeting.SetDescription(description);
+            if(meeting._StartDate != startDate)
+                meeting.UpdateStartDate(startDate);
+            if(meeting._EndDate != endDate)
+                meeting.UpdateEndDate(endDate);
+
+            await _repository.Update(meeting);
+        }
+    }
+}
---
+++ VirtualOffice.Application/Commands/MeetingCommands/UpdateMeeting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Shared.Abstractions.Commands;
+
+namespace VirtualOffice.Application.Commands.MeetingCommands
+{
+    public record UpdateMeeting(Guid guid, string title, string description, DateTime startDate, DateTime endDate) : ICommand;
+
+}
