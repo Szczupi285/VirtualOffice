b7c904e9ff4a13ab04cfdd372cd018b05cf9163d
unknown <bartekbiexd@gmail.com>
2024-07-09 23:00:14 +0200

    Created AddOrganizationUsers command and handler

---
+++ VirtualOffice.Application/Commands/Handlers/OrganizationHandlers/AddOrganizationUsersHandler.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Application.Commands.OrganizationCommands;
+using VirtualOffice.Application.Exceptions.Organization;
+using VirtualOffice.Application.Services;
+using VirtualOffice.Domain.Repositories;
+
+namespace VirtualOffice.Application.Commands.Handlers.OrganizationHandlers
+{
+    public class AddOrganizationUsersHandler : IRequestHandler<AddOrganizationUsers>
+    {
+
+        public IOrganizationRepository _repository;
+        public IOrganizationReadService _readService;
+
+        public AddOrganizationUsersHandler(IOrganizationRepository repository, IOrganizationReadService readService)
+        {
+            _repository = repository;
+            _readService = readService;
+        }
+
+        public async Task Handle(AddOrganizationUsers request, CancellationToken cancellationToken)
+        {
+            if (!await _readService.ExistsByIdAsync(request.Id))
+                throw new OrganizationDoesNotExistsException(request.Id);
+
+            var org = await _repository.GetById(request.Id);
+
+            org.AddRangeUsers(request.Users);
+            await _repository.Update(org);
+            await _repository.SaveAsync(cancellationToken);
+
+
+        }
+    }
+}
--- VirtualOffice.Application/Services/IOrganizationReadService.cs
+++ VirtualOffice.Application/Services/IOrganizationReadService.cs
@@ -3,23 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using VirtualOffice.Domain.Repositories;
 
 namespace VirtualOffice.Application.Services
 {
     public interface IOrganizationReadService
     {
-
-
-        public IOrganizationRepository _repository;
-        public IOrganizationReadService _readService;
-
-        public CreateNoteHandler(INoteRepository repository, INoteReadService noteReadService)
-        {
-            _repository = repository;
-            _readService = noteReadService;
-        }
-
         Task<bool> ExistsByIdAsync(Guid id);
     }
 }
