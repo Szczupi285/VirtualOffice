02102980c26130212e03ee774e29eb5b1b9d7ee0
unknown <bartekbiexd@gmail.com>
2024-07-09 16:37:28 +0200

    Created DeleteOrganization command and handler

---
+++ VirtualOffice.Application/Commands/Handlers/OrganizationHandlers/DeleteOrganizationHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Application.Commands.OrganizationCommands;
+using VirtualOffice.Application.Exceptions.Organization;
+using VirtualOffice.Application.Services;
+using VirtualOffice.Domain.Repositories;
+
+namespace VirtualOffice.Application.Commands.Handlers.OrganizationHandlers
+{
+    public class DeleteOrganizationHandler : IRequestHandler<DeleteOrganization>
+    {
+
+        public IOrganizationRepository _repository;
+        public IOrganizationReadService _readService;
+
+        public DeleteOrganizationHandler(IOrganizationRepository repository, IOrganizationReadService readService)
+        {
+            _repository = repository;
+            _readService = readService;
+        }
+
+        public async Task Handle(DeleteOrganization request, CancellationToken cancellationToken)
+        {
+            if (!await _readService.ExistsByIdAsync(request.Id))
+                throw new OrganizationDoesNotExistsException(request.Id);
+
+            await _repository.Delete(request.Id);
+            await _repository.SaveAsync(cancellationToken);
+        }
+    }
+}
---
+++ VirtualOffice.Application/Exceptions/Organization/OrganizationDoesNotExistsException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Shared.Abstractions.Exceptions;
+
+namespace VirtualOffice.Application.Exceptions.Organization
+{
+    public class OrganizationDoesNotExistsException : VirtualOfficeException
+    {
+        public OrganizationDoesNotExistsException(Guid id) : base($"Organization with Id: {id} does not exist")
+        {
+        }
+    }
+}
