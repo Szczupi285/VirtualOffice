d58299f8b426684767fdf48e0bb06a6c684ae0a7
unknown <bartekbiexd@gmail.com>
2024-07-09 23:23:28 +0200

    Created DeleteOrganizationOfficeUsers Command and Handler

---
+++ VirtualOffice.Application/Commands/OrganizationCommands/DeleteOrganizationOfficeUsers.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Domain.Entities;
+
+namespace VirtualOffice.Application.Commands.OrganizationCommands
+{
+    public record DeleteOrganizationOfficeUsers(Guid OrganizationId, Guid OfficeId ,ICollection<ApplicationUser> Users) : IRequest;
+
+}
