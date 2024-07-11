1644df61027050c124c93cf6337f0c20e60cb996
unknown <bartekbiexd@gmail.com>
2024-07-11 16:47:51 +0200

    Creted DeletePrivateChatRoom command and handler

--- VirtualOffice.Application/Exceptions/PrivateChatRoom/PrivateChatRoomDoesNotExistException.cs
+++ VirtualOffice.Application/Exceptions/PrivateChatRoom/PrivateChatRoomDoesNotExistException.cs
@@ -7,9 +7,9 @@ using VirtualOffice.Shared.Abstractions.Exceptions;
 
 namespace VirtualOffice.Application.Exceptions.PrivateChatRoom
 {
-    public class PrivateChatRoomAlreadyExistException : VirtualOfficeException
+    public class PrivateChatRoomDoesNotExistException : VirtualOfficeException
     {
-        public PrivateChatRoomAlreadyExistException(Guid id) : base($"Private chat room with Id: {id} already exist")
+        public PrivateChatRoomDoesNotExistException(Guid Id) : base($"Chat room with {Id} does not exist")
         {
         }
     }
