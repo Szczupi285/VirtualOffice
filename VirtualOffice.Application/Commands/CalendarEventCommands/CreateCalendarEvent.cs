353e5d3a39ac52cb4d7b1564372df365095d278a
unknown <bartekbiexd@gmail.com>
2024-07-11 19:58:53 +0200

    Refactored CreateCalendarEvent command and handler

--- VirtualOffice.Application/Commands/CalendarEventCommands/CreateCalendarEvent.cs
+++ VirtualOffice.Application/Commands/CalendarEventCommands/CreateCalendarEvent.cs
@@ -3,7 +3,7 @@ using VirtualOffice.Domain.Entities;
 
 namespace VirtualOffice.Application.Commands.CalendarEventCommands
 {
-    public record CreateCalendarEvent(Guid Guid, string Title, string Description, HashSet<ApplicationUser> AssignedEmployees, DateTime StartDate, DateTime EndDate) : IRequest;
+    public record CreateCalendarEvent(string Title, string Description, HashSet<ApplicationUser> AssignedEmployees, DateTime StartDate, DateTime EndDate) : IRequest;
 
 
 }
