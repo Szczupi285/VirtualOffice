5794b9b05f8096d81670ab9d0719014572bba119
unknown <bartekbiexd@gmail.com>
2024-07-11 19:55:01 +0200

    Created 'CreatePublicChatRoom' command and handler

---
+++ VirtualOffice.Application/Commands/Handlers/PublicChatRoomHandlers/CreatePublicChatRoomHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using VirtualOffice.Application.Commands.PublicChatRoomCommands;
+using VirtualOffice.Application.Exceptions.PublicChatRoom;
+using VirtualOffice.Application.Services;
+using VirtualOffice.Domain.Entities;
+using VirtualOffice.Domain.Repositories;
+
+namespace VirtualOffice.Application.Commands.Handlers.PublicChatRoomHandlers
+{
+    public class CreatePublicChatRoomHandler : IRequest<CreatePublicChatRoom>
+    {
+        public IPublicChatRoomRepository _repository;
+        public IPublicChatRoomReadService _readService;
+
+        public CreatePublicChatRoomHandler(IPublicChatRoomRepository repository, IPublicChatRoomReadService noteReadService)
+        {
+            _repository = repository;
+            _readService = noteReadService;
+        }
+
+
+        public async Task Handle(CreatePublicChatRoom request, CancellationToken cancellationToken)
+        {
+            PublicChatRoom pcr = new(Guid.NewGuid(), request.Participants, request.Messages, request.Name);
+
+            await _repository.Add(pcr);
+            await _repository.SaveAsync(cancellationToken);
+        }
+    }
+}
---
+++ VirtualOffice.Application/Commands/PublicChatRoomCommands/CreatePublicChatRoom.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Domain.Entities;
+using VirtualOffice.Domain.ValueObjects.ChatRoom;
+
+namespace VirtualOffice.Application.Commands.PublicChatRoomCommands
+{
+    public record CreatePublicChatRoom(HashSet<ApplicationUser> Participants, SortedSet<Message> Messages, string Name) : IRequest;
+}
