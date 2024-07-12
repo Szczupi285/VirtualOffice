57d88f16d0953fc419f681e1eb8916760cdaade8
unknown <bartekbiexd@gmail.com>
2024-07-12 17:44:16 +0200

    Created Public/Private DocumentCaretakers

---
+++ VirtualOffice.Application/Services/PrivateDocumentCaretaker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Domain.Abstractions;
+using VirtualOffice.Domain.Entities;
+
+namespace VirtualOffice.Application.Services
+{
+    public class PrivateDocumentCaretaker
+    {
+        private readonly Stack<PrivateDocumentMemento> _mementoStack = new();
+
+        public void SaveState(PrivateDocument document)
+        {
+            _mementoStack.Push(document.SaveToMemento());
+        }
+
+        public void RestoreState(PrivateDocument document)
+        {
+            if (_mementoStack.Count > 0)
+            {
+                document.RestoreFromMemento(_mementoStack.Pop());
+            }
+        }
+    }
+}
---
+++ VirtualOffice.Application/Services/PublicDocumentCaretaker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Domain.Entities;
+
+namespace VirtualOffice.Application.Services
+{
+    public class PublicDocumentCaretaker
+    {
+        private readonly Stack<PublicDocumentMemento> _mementoStack = new();
+
+        public void SaveState(PublicDocument document)
+        {
+            _mementoStack.Push(document.SaveToMemento());
+        }
+
+        public void RestoreState(PublicDocument document)
+        {
+            if (_mementoStack.Count > 0)
+            {
+                document.RestoreFromMemento(_mementoStack.Pop());
+            }
+        }
+    }
+}
