adf577c9f7aa4801ed10ef21aed47307fb5b659b
unknown <gosgie02@wp.pl>
2024-07-12 16:05:38 +0200

    created UpdateUser command

---
+++ VirtualOffice.Application/Commands/Handlers/UserHandlers/UpdateUserHandler.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Application.Commands.UserCommands;
+using VirtualOffice.Application.Exceptions.User;
+using VirtualOffice.Application.Services;
+using VirtualOffice.Domain.Repositories;
+
+namespace VirtualOffice.Application.Commands.Handlers.UserHandlers
+{
+    public class UpdateUserHandler : IRequestHandler<UpdateUser>
+    {
+        public IUserRepository _repository;
+        public IUserReadService _readService;
+
+        public UpdateUserHandler(IUserRepository repository, IUserReadService readService)
+        {
+            _repository = repository;
+            _readService = readService;
+        }
+
+        public async Task Handle(UpdateUser request, CancellationToken cancellationToken)
+        {
+            var (Id, Name, Surname, Permissions) = request;
+            if (!await _readService.ExistsByIdAsync(request.Id))
+            {
+                throw new UserDoesNotExistException(request.Id);
+            }
+
+            var user = await _repository.GetById(Id);
+
+            
+        }
+    }
+}
---
+++ VirtualOffice.Application/Commands/UserCommands/UpdateUser.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Domain.Consts;
+
+namespace VirtualOffice.Application.Commands.UserCommands
+{
+    public record UpdateUser(Guid Id, string Name, string Surname, PermissionsEnum Permissions) : IRequest;
+    
+}
