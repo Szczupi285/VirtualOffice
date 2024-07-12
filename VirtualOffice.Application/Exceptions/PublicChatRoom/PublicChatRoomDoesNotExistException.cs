b51960826205019cd0177c600074fa5697ec8370
unknown <bartekbiexd@gmail.com>
2024-07-12 15:44:06 +0200

    Created: DeletePublicChatRoom, SendPublicMessage, UpdatePublicChatRoomName commands and handlers

---
+++ VirtualOffice.Application/Exceptions/PublicChatRoom/PublicChatRoomDoesNotExistException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Shared.Abstractions.Exceptions;
+
+namespace VirtualOffice.Application.Exceptions.PublicChatRoom
+{
+    public class PublicChatRoomDoesNotExistException : VirtualOfficeException
+    {
+        public PublicChatRoomDoesNotExistException(Guid Id) : base($"Public chat room with Id: {Id} does not exist")
+        {
+        }
+    }
+}
