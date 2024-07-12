fc7dc6ed663c11586406064c0952f961e4b96980
unknown <bartekbiexd@gmail.com>
2024-07-12 15:59:27 +0200

    Created AddChatRoomParticipants command and handler

---
+++ VirtualOffice.Application/Commands/Handlers/PublicChatRoomHandlers/AddChatParticipantsHandler.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Application.Commands.PublicChatRoomCommands;
+using VirtualOffice.Application.Exceptions.PublicChatRoom;
+using VirtualOffice.Application.Services;
+using VirtualOffice.Domain.Repositories;
+
+namespace VirtualOffice.Application.Commands.Handlers.PublicChatRoomHandlers
+{
+    public class AddChatParticipantsHandler : IRequestHandler<AddChatParticipants>
+    {
+        public IPublicChatRoomRepository _repository;
+        public IPublicChatRoomReadService _readService;
+
+        public AddChatParticipantsHandler(IPublicChatRoomRepository repository, IPublicChatRoomReadService readService)
+        {
+            _repository = repository;
+            _readService = readService;
+        }
+
+        public async Task Handle(AddChatParticipants request, CancellationToken cancellationToken)
+        {
+            if (!await _readService.ExistsByIdAsync(request.Id))
+                throw new PublicChatRoomDoesNotExistException(request.Id);
+
+            var pcr = await _repository.GetById(request.Id);
+
+            pcr.AddRangeParticipants(request.Participants);
+
+            await _repository.Update(pcr);
+            await _repository.SaveAsync(cancellationToken);
+        }
+    }
+}
