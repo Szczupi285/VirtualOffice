838f6ee7d19c43d5d36fff450894f5fd96f408e5
unknown <bartekbiexd@gmail.com>
2024-07-19 18:53:44 +0200

    Created AddPublicDocumentAttachmend command and handler

---
+++ VirtualOffice.Application/Commands/Handlers/PublicDocumentHandlers/AddPublicDocumentAttachmentHandler.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Application.Commands.PrivateDocumentCommands;
+using VirtualOffice.Application.Commands.PublicDocumentCommands;
+using VirtualOffice.Application.Exceptions.PrivateDocument;
+using VirtualOffice.Application.Services;
+using VirtualOffice.Domain.Repositories;
+
+namespace VirtualOffice.Application.Commands.Handlers.PublicDocumentHandlers
+{
+    public class AddPublicDocumentAttachmentHandler : IRequestHandler<AddPublicDocumentAttachment>
+    {
+        IPrivateDocumentRepository _repository;
+        IPrivateDocumentReadService _readService;
+
+        public AddPublicDocumentAttachmentHandler(IPrivateDocumentRepository repository, IPrivateDocumentReadService readService)
+        {
+            _repository = repository;
+            _readService = readService;
+        }
+
+        public async Task Handle(AddPublicDocumentAttachment request, CancellationToken cancellationToken)
+        {
+            if (!await _readService.ExistsByIdAsync(request.Id))
+                throw new PrivateDocumentDoesNotExistException(request.Id);
+
+            var pubDoc = await _repository.GetById(request.Id);
+            pubDoc.AddNewAttachment(request.AttachmentFilePath);
+
+            await _repository.Update(pubDoc);
+            await _repository.SaveAsync(cancellationToken);
+        }
+    }
+}
