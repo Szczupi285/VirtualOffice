6f3248d0dbf4f8ffdec5dc58c07e66e894331377
unknown <bartekbiexd@gmail.com>
2024-07-27 19:37:11 +0200

    Created ApplicationUserDTO and CalendarEventDTO

---
+++ VirtualOffice.Application/DTO/ApplicationUserDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Domain.ValueObjects.ApplicationUser;
+
+namespace VirtualOffice.Application.DTO
+{
+    public class ApplicationUserDTO
+    {
+        Guid Id { get; }
+        public string _Name { get; }
+        public string _Surname { get; }
+    }
+}
---
+++ VirtualOffice.Application/DTO/CalendarEventDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Domain.Entities;
+using VirtualOffice.Domain.ValueObjects.ScheduleItem;
+
+namespace VirtualOffice.Application.DTO
+{
+    public class CalendarEventDTO
+    {
+        public Guid Id { get; }
+        public string _Title { get; }
+        public string _Description { get; }
+        public HashSet<ApplicationUserDTO> _AssignedEmployees { get; }
+        public DateTime _StartDate { get; }
+        public DateTime _EndDate { get; }
+    }
+}
