96b2e1ba03dc7069847a86d366521f0b9b7e47eb
unknown <bartekbiexd@gmail.com>
2024-07-27 19:46:34 +0200

    Created GetCalendarEventById query

---
+++ VirtualOffice.Application/Queries/GetCalendarEvent.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Application.DTO;
+
+namespace VirtualOffice.Application.Queries
+{
+    public record GetCalendarEventById(Guid Id) : IRequest<CalendarEventDTO>;
+}
