a0ad719ae17f75ed85bb494089dd798b9867683f
unknown <bartekbiexd@gmail.com>
2024-07-27 19:53:57 +0200

    Created GetAllFutureCalendarEventsForUser query

--- VirtualOffice.Application/Queries/CalendarEvent/GetAllFutureCalendarEventsForUser.cs
+++ VirtualOffice.Application/Queries/CalendarEvent/GetAllFutureCalendarEventsForUser.cs
@@ -8,5 +8,5 @@ using VirtualOffice.Application.DTO;
 
 namespace VirtualOffice.Application.Queries.CalendarEvent
 {
-    public record GetAllCalendarEventsForUser(Guid UserId) : IRequest<IEnumerable<CalendarEventDTO>>;
+    public record GetAllFutureCalendarEventsForUser(Guid UserId) : IRequest<IEnumerable<CalendarEventDTO>>;
 }
