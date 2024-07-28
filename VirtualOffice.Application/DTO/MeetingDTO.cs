734373cfea9a2f8672d3cb31f7619535ad50990c
unknown <bartekbiexd@gmail.com>
2024-07-28 22:20:41 +0200

    Created MeetingDTO and GetAllMeetingsForUser query

---
+++ VirtualOffice.Application/DTO/MeetingDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualOffice.Application.DTO
+{
+    public class MeetingDTO
+    {
+        Guid Id { get; }
+        public string _Title { get; }
+        public string _Description { get; }
+        public List<ApplicationUserDTO> _AssignedEmployees { get; }
+        public DateTime _StartDate { get; }
+        public DateTime _EndDate { get; }
+
+    }
+}
