94c7ef177dbf54683670dda4a55ce8223711b16c
unknown <bartekbiexd@gmail.com>
2024-07-28 23:40:02 +0200

    Created queries for privateChatRoom

---
+++ VirtualOffice.Application/DTO/MessageDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualOffice.Application.DTO
+{
+    public class MessageDTO
+    {
+        public Guid MessageId { get; init; }
+        public Guid SenderID { get; init; }
+        public string Message { get; init; }
+        public DateTime SendDate { get; init; }
+    }
+}
