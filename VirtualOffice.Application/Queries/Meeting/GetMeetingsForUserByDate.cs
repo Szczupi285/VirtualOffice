949d049893d507532acf2dbf221b7136b3d608fc
unknown <bartekbiexd@gmail.com>
2024-07-28 22:26:10 +0200

    Created GetMeetingsForUserByDate query

---
+++ VirtualOffice.Application/Queries/Meeting/GetMeetingsForUserByDate.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Application.DTO;
+
+namespace VirtualOffice.Application.Queries.Meeting
+{
+    public record GetMeetingsForUserByDate(Guid userId, DateTime StartDate, DateTime EndDate) : IRequest<IEnumerable<MeetingDTO>>;
+}
