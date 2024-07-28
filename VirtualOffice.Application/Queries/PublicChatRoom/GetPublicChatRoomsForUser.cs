0e4c1e74bb7d99d1859a861c769650bae55be454
unknown <bartekbiexd@gmail.com>
2024-07-29 00:53:15 +0200

    Created GetPublicChatRoomsForUser

---
+++ VirtualOffice.Application/Queries/PublicChatRoom/GetPublicChatRoomsForUser.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Application.DTO;
+
+namespace VirtualOffice.Application.Queries.PublicChatRoom
+{
+    public class GetPublicChatRoomsForUser : IRequest<IEnumerable<PublicChatRoomDTO>>
+    {
+    }
+}
