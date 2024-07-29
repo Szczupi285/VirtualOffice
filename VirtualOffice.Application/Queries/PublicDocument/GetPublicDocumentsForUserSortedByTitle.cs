be4876e7ed9ad263129d8d8e8f28d8421c9ddd47
unknown <bartekbiexd@gmail.com>
2024-07-29 19:51:13 +0200

    Created GetPublicDocumentForUserSortedByTitle query

---
+++ VirtualOffice.Application/Queries/PublicDocument/GetPublicDocumentsForUserSortedByTitle.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Application.DTO.PublicDocument;
+
+namespace VirtualOffice.Application.Queries.PublicDocument
+{
+    public record GetPublicDocumentsForUserSortedByTitle(Guid UserId, string Title) : IRequest<IEnumerable<PublicDocumentTitleDTO>>;
+}
