03c8891633864e89c3b578f06b728a648031793c
unknown <bartekbiexd@gmail.com>
2024-07-29 19:54:14 +0200

    Created GetPublicDocumentForUserCreatedBefore/After queries

---
+++ VirtualOffice.Application/Queries/PublicDocument/GetPublicDocumentForUserCreatedAfter.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Application.DTO.PrivateDocument;
+using VirtualOffice.Application.DTO.PublicDocument;
+
+namespace VirtualOffice.Application.Queries.PublicDocument
+{
+    public record GerPublicDocumentForUserCreatedAfter(Guid UserId, DateTime DateTime) : IRequest<IEnumerable<PublicDocumentTitleDTO>>;
+}
