22cfe7be41126b66c0ae7985b9814a4b09115875
unknown <bartekbiexd@gmail.com>
2024-09-18 15:17:15 +0200

    Created EmployeeTaskService

---
+++ VirtualOffice.Infrastructure/MongoDb/Services/EmployeeTaskService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Infrastructure.abstractions;
+using VirtualOffice.Infrastructure.EF.Models;
+using VirtualOffice.Infrastructure.EF.Models.ReadDatabaseSettings;
+
+namespace VirtualOffice.Infrastructure.MongoDb.Services
+{
+    public class EmployeeTaskService : AbstractModelService<EmployeeTaskReadModel>
+    {
+        public EmployeeTaskService(IOptions<ReadDatabaseSettings> ReadDatabaseSettings)
+            : base(ReadDatabaseSettings, ReadDatabaseSettings.Value.EmployeeTasksCollectionName)
+        {
+        }
+    }
+}
