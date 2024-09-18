9a07d9c9d358c9979b44efd0f6308deae793b982
unknown <bartekbiexd@gmail.com>
2024-09-18 10:42:51 +0200

    Created EmployeeTaskReadModel

--- VirtualOffice.Infrastructure/EF/Models/EmployeeTaskReadModel.cs
+++ VirtualOffice.Infrastructure/EF/Models/EmployeeTaskReadModel.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VirtualOffice.Domain.Consts;
 
 namespace VirtualOffice.Infrastructure.EF.Models
 {
-    public class CalendarEventReadModel
+    public class EmployeeTaskReadModel
     {
         public string Id { get; set; }
         public int Version { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public List<UserReadModel> AssignedEmployees { get; set; }
+        public EmployeeTaskPriorityEnum Priority { get; set; }
+        public EmployeeTaskStatusEnum Status { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
     }
