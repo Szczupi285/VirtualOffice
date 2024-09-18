373c3ed79cb750d329e4b154cafc03fa0c5dba09
unknown <bartekbiexd@gmail.com>
2024-09-18 10:49:26 +0200

    Created MeetingReadModel and MessageReadModel

---
+++ VirtualOffice.Infrastructure/EF/Models/MeetingReadModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualOffice.Infrastructure.EF.Models
+{
+    public class MeetingReadModel
+    {
+        public string Id { get; set; }
+        public int Version { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public List<UserReadModel> AssignedEmployees { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ VirtualOffice.Infrastructure/EF/Models/MessageReadModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualOffice.Infrastructure.EF.Models
+{
+    public class MessageReadModel
+    {
+        public string Id { get; set; }
+        public UserReadModel Sender { get; set; }
+        public string Content { get; set; }
+    }
+}
