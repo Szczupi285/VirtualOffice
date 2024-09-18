0b9cf33e5cdf5d043c0af4a1d3212de2c04d609b
unknown <bartekbiexd@gmail.com>
2024-09-18 10:04:45 +0200

    Created CalendarEventReadModel and initial UserReadModel

---
+++ VirtualOffice.Infrastructure/EF/Models/CalendarEventReadModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualOffice.Infrastructure.EF.Models
+{
+    public class CalendarEventReadModel
+    {
+        public Guid Id { get; set; }
+        public int Version { get; set; }
+        public string Description { get; set; }
+        public ICollection<UserReadModel> Users { get; set; }
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ VirtualOffice.Infrastructure/EF/Models/UserReadModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Domain.Consts;
+
+namespace VirtualOffice.Infrastructure.EF.Models
+{
+    public class UserReadModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public PermissionsEnum Permissions { get; set; }
+        public CalendarEventReadModel CalendarEventReadModel { get; set; }
+    }
+}
