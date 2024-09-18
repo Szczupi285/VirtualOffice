9dba6403eeffab970bc09d44c27e95f0b291fba2
unknown <bartekbiexd@gmail.com>
2024-09-18 11:27:19 +0200

    PrivateDocumentReadModel

---
+++ VirtualOffice.Infrastructure/EF/Models/PrivateDocumentReadModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualOffice.Infrastructure.EF.Models
+{
+    public class PrivateDocumentReadModel
+    {
+        public string Id { get; set; }
+        public int Version { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public List<string> AttachmentFilePath { get; set; }
+    }
+}
