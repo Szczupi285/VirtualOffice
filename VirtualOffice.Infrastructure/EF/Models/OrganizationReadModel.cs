4adca211f6ded78dc5235c1e94ec0fdd0915cc81
unknown <bartekbiexd@gmail.com>
2024-09-18 11:14:26 +0200

    Created Organization/Subscription read models

---
+++ VirtualOffice.Infrastructure/EF/Models/OrganizationReadModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualOffice.Infrastructure.EF.Models
+{
+    public class OrganizationReadModel
+    {
+        public string Id { get; set; }
+        public int Version { get; set; }
+        public string Name { get; set; }
+        public ushort UserLimit { get; set; }
+        public List<OfficeReadModel> Offices { get; set; }
+        public List<UserReadModel> Users { get; set; }
+        public SubscriptionReadModel Subscription { get; set; }
+    }
+}
---
+++ VirtualOffice.Infrastructure/EF/Models/SubscriptionReadModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualOffice.Infrastructure.EF.Models
+{
+    public class SubscriptionReadModel
+    {
+        public string Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal Fee { get; set; }
+        public bool IsPayed { get; set; }
+    }
+}
