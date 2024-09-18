7c57d45bc1b3afa8a38752b9e5555c42c174caa1
unknown <bartekbiexd@gmail.com>
2024-09-18 15:54:33 +0200

    Created PublicDocumentService and PublicChatRoomService

---
+++ VirtualOffice.Infrastructure/MongoDb/Services/PublicDocumentsService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Infrastructure.abstractions;
+using VirtualOffice.Infrastructure.EF.Models;
+using VirtualOffice.Infrastructure.EF.Models.ReadDatabaseSettings;
+
+namespace VirtualOffice.Infrastructure.MongoDb.Services
+{
+    public class PublicDocumentsService : AbstractModelService<PublicDocumentReadModel>
+    {
+        public PublicDocumentsService(IOptions<ReadDatabaseSettings> ReadDatabaseSettings)
+            : base(ReadDatabaseSettings, ReadDatabaseSettings.Value.PublicDocumentsCollectionName)
+        {
+        }
+    }
+}
