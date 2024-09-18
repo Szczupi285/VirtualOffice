1170217d22ba03bd46f04c411dfc90b5a99fd721
unknown <bartekbiexd@gmail.com>
2024-09-18 11:19:57 +0200

    Created PRivateChatRoomReadModel

---
+++ VirtualOffice.Infrastructure/EF/Models/PrivateChatRoomReadModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualOffice.Infrastructure.EF.Models
+{
+    public class PrivateChatRoomReadModel
+    {
+        public string Id { get; set; }
+        public int Version { get; set; }
+        public List<UserReadModel> Users { get; set; }
+        public List<MessageReadModel> Messages { get; set; }
+    }
+}
