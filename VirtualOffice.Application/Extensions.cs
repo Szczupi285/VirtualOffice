74f95e12f97c0a2434a217b175c3b45af60d7c98
unknown <bartekbiexd@gmail.com>
2024-09-18 08:47:24 +0200

    Created Extensions class

---
+++ VirtualOffice.Application/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualOffice.Application
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddServicesCollection(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IApplicationBuilder UseServicesCollection(this IApplicationBuilder app)
+        {
+            return app;
+        }
+    }
+}
