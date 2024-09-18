92009d0a707ee58f0e69f49bb8eb772142ad78e6
unknown <bartekbiexd@gmail.com>
2024-09-18 11:05:38 +0200

    Created OfficeReadModel

---
+++ VirtualOffice.Infrastructure/EF/Models/OfficeReadModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualOffice.Infrastructure.EF.Models
+{
+    public class OfficeReadModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<UserReadModel> Users { get; set; }
+    }
+}
