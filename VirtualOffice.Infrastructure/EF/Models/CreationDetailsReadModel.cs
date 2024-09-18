6b66414d0d190096d28f1ad7330d59a1fd29b016
unknown <bartekbiexd@gmail.com>
2024-09-18 11:38:13 +0200

    Created PublicDocumentReadModel

---
+++ VirtualOffice.Infrastructure/EF/Models/CreationDetailsReadModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualOffice.Infrastructure.EF.Models
+{
+    public class CreationDetailsReadModel
+    {
+        public string UserId { get; set; }
+        public DateTime DocumentCreationDate { get; set; }
+    }
+}
---
+++ VirtualOffice.Infrastructure/EF/Models/PublicDocumentReadModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualOffice.Infrastructure.EF.Models
+{
+    public class PublicDocumentReadModel
+    {
+        public string Id { get; set; }
+        public int Version { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreationDate { get; set; }
+        public List<string> AttachmentFilePaths { get; set; }
+        public CreationDetailsReadModel CreationDetails { get; set; }
+        public List<string> EligibleForReadIds { get; set; }
+        public List<string> EligibleForWriteIds { get; set; }
+    }
+}
