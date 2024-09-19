52a7b330ed95a06f859b94ffd5d841389163933f
unknown <bartekbiexd@gmail.com>
2024-09-19 09:44:13 +0200

    Added DbSets to WriteDbContext, created IdentityUser

---
+++ VirtualOffice.Infrastructure/EF/WriteDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Domain.Entities;
+using VirtualOffice.Infrastructure.Identity;
+
+namespace VirtualOffice.Infrastructure.EF
+{
+    public class WriteDbContext : IdentityDbContext<AppIdentityUser, IdentityRole<Guid>, Guid>
+    {
+        public DbSet<CalendarEvent> CalendarEvents { get; set; }
+        public DbSet<EmployeeTask> EmployeeTasks { get; set; }
+        public DbSet<Meeting> Meetings { get; set; }
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<Organization> Organizations { get; set; }
+        public DbSet<PrivateChatRoom> PrivateChatRooms { get; set; }
+        public DbSet<PrivateDocument> PrivateDocuments { get; set; }
+        public DbSet<PublicChatRoom> PublicChatRooms { get; set; }
+        public DbSet<PublicDocument> PublicDocuments { get; set; }
+
+        public WriteDbContext(DbContextOptions<WriteDbContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ VirtualOffice.Infrastructure/Identity/AppIdentityUser.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Domain.Entities;
+using VirtualOffice.Domain.ValueObjects.ApplicationUser;
+
+namespace VirtualOffice.Infrastructure.Identity
+{
+    public class AppIdentityUser : IdentityUser<Guid>
+    {
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public AppIdentityUser()
+        { }
+
+        public AppIdentityUser(ApplicationUser applicationUser)
+        {
+            Id = ApplicationUser.Id.Value;
+        }
+    }
+}
