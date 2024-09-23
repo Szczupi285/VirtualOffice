204aaa604ca2eae3f9c04ba72efd3edc71e28793
unknown <bartekbiexd@gmail.com>
2024-09-23 13:41:09 +0200

    Created PrivateChatRoom repository

---
+++ VirtualOffice.Infrastructure/EF/Repositories/PrivateChatRoomRepository.cs
@@ -0,0 +1,72 @@
+﻿using Amazon.Runtime.Internal;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Domain.Entities;
+using VirtualOffice.Domain.Exceptions.Repositories;
+using VirtualOffice.Domain.Repositories;
+using VirtualOffice.Domain.ValueObjects.AbstractChatRoom;
+
+namespace VirtualOffice.Infrastructure.EF.Repositories
+{
+    public class PrivateChatRoomRepository : IPrivateChatRoomRepository
+    {
+        private readonly WriteDbContext _dbContext;
+
+        public PrivateChatRoomRepository(WriteDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<PrivateChatRoom> GetByIdAsync(ChatRoomId guid)
+            => await _dbContext.PrivateChatRooms
+            .Include(pcr => pcr._Participants)
+            .Include(pcr => pcr._Messages)
+            .FirstOrDefaultAsync(pcr => pcr.Id == guid) ?? throw new PrivateChatRoomNotFoundException(guid);
+
+        public async Task<PrivateChatRoom> GetByIdAsync(ChatRoomId guid, CancellationToken cancellationToken)
+            => await _dbContext.PrivateChatRooms
+            .Include(pcr => pcr._Participants)
+            .Include(pcr => pcr._Messages)
+            .FirstOrDefaultAsync(pcr => pcr.Id == guid, cancellationToken) ?? throw new PrivateChatRoomNotFoundException(guid);
+
+        public async Task AddAsync(PrivateChatRoom chatRoom)
+        {
+            await _dbContext.PrivateChatRooms.AddAsync(chatRoom);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task AddAsync(PrivateChatRoom chatRoom, CancellationToken cancellationToken)
+        {
+            await _dbContext.PrivateChatRooms.AddAsync(chatRoom, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task DeleteAsync(PrivateChatRoom chatRoom)
+        {
+            _dbContext.PrivateChatRooms.Remove(chatRoom);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(PrivateChatRoom chatRoom, CancellationToken cancellationToken)
+        {
+            _dbContext.PrivateChatRooms.Remove(chatRoom);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task UpdateAsync(PrivateChatRoom chatRoom)
+        {
+            _dbContext.PrivateChatRooms.Update(chatRoom);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(PrivateChatRoom chatRoom, CancellationToken cancellationToken)
+        {
+            _dbContext.PrivateChatRooms.Update(chatRoom);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
