45c71c9913ddcccea4d973a59ff83e9b560c1f49
unknown <bartekbiexd@gmail.com>
2024-09-23 15:01:23 +0200

    Created PublicDocumentRepository

---
+++ VirtualOffice.Domain/Exceptions/Repositories/PublicDocumentNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualOffice.Shared.Abstractions.Exceptions;
+
+namespace VirtualOffice.Domain.Exceptions.Repositories
+{
+    public class PublicDocumentNotFoundException : VirtualOfficeException
+    {
+        public PublicDocumentNotFoundException(Guid guid) : base($"Public document with Id: {guid} has not been found")
+        {
+        }
+    }
+}
---
+++ VirtualOffice.Infrastructure/EF/Repositories/PublicDocumentRepository.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using VirtualOffice.Domain.Entities;
+using VirtualOffice.Domain.Exceptions.Repositories;
+using VirtualOffice.Domain.Repositories;
+using VirtualOffice.Domain.ValueObjects.Document;
+
+namespace VirtualOffice.Infrastructure.EF.Repositories
+{
+    public class PublicDocumentRepository : IPublicDocumentRepository
+    {
+        private readonly WriteDbContext _dbContext;
+
+        public PublicDocumentRepository(WriteDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<PublicDocument> GetByIdAsync(DocumentId guid)
+            => await _dbContext.PublicDocuments
+            .Include(pd => pd._attachmentFilePaths)
+            .Include(pd => pd._eligibleForRead)
+            .Include(pd => pd._eligibleForWrite)
+            .FirstOrDefaultAsync(pd => pd.Id == guid) ?? throw new PublicDocumentNotFoundException(guid);
+
+        public async Task<PublicDocument> GetByIdAsync(DocumentId guid, CancellationToken cancellationToken)
+            => await _dbContext.PublicDocuments
+            .Include(pd => pd._attachmentFilePaths)
+            .Include(pd => pd._eligibleForRead)
+            .Include(pd => pd._eligibleForWrite)
+            .FirstOrDefaultAsync(pd => pd.Id == guid) ?? throw new PublicDocumentNotFoundException(guid);
+
+        public async Task AddAsync(PublicDocument publicDocument)
+        {
+            await _dbContext.PublicDocuments.AddAsync(publicDocument);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task AddAsync(PublicDocument publicDocument, CancellationToken cancellationToken)
+        {
+            await _dbContext.PublicDocuments.AddAsync(publicDocument, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task DeleteAsync(PublicDocument publicDocument)
+        {
+            _dbContext.PublicDocuments.Remove(publicDocument);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(PublicDocument publicDocument, CancellationToken cancellationToken)
+        {
+            _dbContext.PublicDocuments.Remove(publicDocument);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task UpdateAsync(PublicDocument publicDocument)
+        {
+            _dbContext.PublicDocuments.Update(publicDocument);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(PublicDocument publicDocument, CancellationToken cancellationToken)
+        {
+            _dbContext.PublicDocuments.Update(publicDocument);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
