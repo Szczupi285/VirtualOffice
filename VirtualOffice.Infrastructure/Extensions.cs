a9519fc7233c2aecc0598137aed9f6c77b9ea818
unknown <bartekbiexd@gmail.com>
2024-09-26 15:43:50 +0200

    Created RabbitMq configuration required to connect MassTransit library to RabbitMQ

--- VirtualOffice.Infrastructure/Extensions.cs
+++ VirtualOffice.Infrastructure/Extensions.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using MassTransit;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using VirtualOffice.Infrastructure.MongoDb.Services;
+using VirtualOffice.Infrastructure.RabbitMQ;
 
 namespace VirtualOffice.Infrastructure
 {
@@ -19,6 +22,28 @@ namespace VirtualOffice.Infrastructure
             services.AddSingleton<PrivateChatRoomsService>();
             services.AddSingleton<PublicChatRoomsService>();
             services.AddSingleton<PublicDocumentsService>();
+
+            // fetch data from appsettings.json
+            services.Configure<RabbitMQSettings>(
+                configuration.GetSection("RabbitMQ"));
+
+            services.AddSingleton(s
+            => s.GetRequiredService<IOptions<RabbitMQSettings>>().Value);
+
+            services.AddMassTransit(c =>
+            {
+                c.SetKebabCaseEndpointNameFormatter();
+
+                c.UsingRabbitMq((context, configurator) =>
+                {
+                    RabbitMQSettings settings = context.GetRequiredService<RabbitMQSettings>();
+                    configurator.Host(new Uri(settings.Host), h =>
+                    {
+                        h.Username(settings.Username);
+                        h.Password(settings.Password);
+                    });
+                });
+            });
 
             return services;
         }
---
+++ VirtualOffice.Infrastructure/RabbitMQ/RabbitMQSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualOffice.Infrastructure.RabbitMQ
+{
+    public sealed class RabbitMQSettings
+    {
+        public string Host { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
