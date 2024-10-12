a492177b294811ad910b1b5e2dc24f4f4690235a
unknown <bartekbiexd@gmail.com>
2024-10-12 13:25:31 +0200

    Created CalendarEventEmployeesRemovedConsumer

---
+++ VirtualOffice.Infrastructure/RabbitMQ/Consumers/CalendarEventEmployeesRemovedConsumer.cs
@@ -0,0 +1,16 @@
+﻿using MassTransit;
+using VirtualOffice.Application.IntegrationEvents;
+using VirtualOffice.Infrastructure.MongoDb.Services;
+
+namespace VirtualOffice.Infrastructure.RabbitMQ.Consumers
+{
+    public class CalendarEventEmployeesRemovedConsumer : IConsumer<CalendarEventEmployeesRemovedIntegrationEvent>
+    {
+        private readonly CalendarEventsService _calendarEventsService;
+
+        public async Task Consume(ConsumeContext<CalendarEventEmployeesRemovedIntegrationEvent> context)
+        {
+            await _calendarEventsService.UpdateAsync(context.Message.Id, context.Message);
+        }
+    }
+}
