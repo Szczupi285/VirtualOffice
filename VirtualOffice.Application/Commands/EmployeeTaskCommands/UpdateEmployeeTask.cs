ee5117c121914dd45921208a0454023f9d67587e
unknown <bartekbiexd@gmail.com>
2024-10-16 18:02:25 +0200

    Deleted unused command/commandHandler

--- VirtualOffice.Application/Commands/EmployeeTaskCommands/UpdateEmployeeTask.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-using VirtualOffice.Domain.Consts;
-
-namespace VirtualOffice.Application.Commands.EmployeeTaskCommands
-{
-    public record UpdateEmployeeTask(Guid Id, string Title, string Description,
-        DateTime EndDate, EmployeeTaskStatusEnum Status, EmployeeTaskPriorityEnum Priority) : IRequest;
-
-}
